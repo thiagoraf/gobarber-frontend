3263efd8f6f52669376b6f1ad01d16a0e9500308
Thiago Rossetto Afonso <thiago.rossetto.afonso@gmail.com>
2020-05-06 17:08:53 -0300

    Componentização do button e input; instalação do Yup para validação.

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,49 @@
+import styled, { css } from "styled-components";
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  color: #666360;
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  width: 100%;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  input {
+    flex: 1;
+    border: 0;
+    background: transparent;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,65 @@
+import React, { useCallback } from "react";
+import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import * as Yup from "yup";
+
+import logoImg from "../../assets/logo.svg";
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+import { Container, Content, Background } from "./styles";
+
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome obrigatório"),
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido."),
+        password: Yup.string().min(6, "No mínimo 5 dígitos"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input icon={FiUser} name="name" placeholder="Name" />
+
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
